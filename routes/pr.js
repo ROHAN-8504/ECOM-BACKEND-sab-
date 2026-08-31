4fb36c92a5ac6af837346d8370b690083f1d5b38
ROHAN-8504 <203j1a4207@raguinstech.com>
2026-08-31 09:07:19 +0530

    new

--- routes/pr.js
+++ routes/pr.js
@@ -1,11 +1,13 @@
 const {bulkproduct,fetchallproducts,deleteproduct,updateproduct,serverstatus}=require('../controllers/pc')
 const express=require('express')
+const authmiddleware=require('../middlewares/authmiddleware')
+const authorizemiddleware=require('../middlewares/authorizemiddleware')
 let router=express.Router()
 
-router.post('/',bulkproduct)
-router.get('/',fetchallproducts)
+router.post('/',authmiddleware,authorizemiddleware('products:create'),bulkproduct)
+router.get('/',authmiddleware,authorizemiddleware('products:read'),fetchallproducts)
 router.get('/status',serverstatus)
-router.delete('/:id',deleteproduct)
-router.put('/:id',updateproduct)
+router.delete('/:id',authmiddleware,authorizemiddleware('products:delete'),deleteproduct)
+router.put('/:id',authmiddleware,authorizemiddleware('products:update'),updateproduct)
 
 module.exports=router
